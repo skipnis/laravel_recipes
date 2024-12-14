7b784ad3ad979f3f7e2eee653bb75c8d0ff86d75
kipnisalex <kipnis.work@gmail.com>
2024-12-14 17:08:47 +0300

    Instruction repository & service

---
+++ app/Services/InstructionServiceInterface.php
@@ -0,0 +1,10 @@
+<?php
+// app/Services/InstructionServiceInterface.php
+
+namespace App\Services;
+
+interface InstructionServiceInterface extends ServiceInterface
+{
+    // Если нужно добавить специфичные методы для инструкций, например, для фильтрации
+    // public function getInstructionsByRecipeId($recipeId);
+}
